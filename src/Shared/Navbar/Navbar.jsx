6888ef8a75bc117abc43579f6490a2ba1533e3f6
rakibaakter <rakibaakter635@gmail.com>
2023-12-24 21:24:00 +0600

    added login logout feature in navbar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,17 +1,53 @@
 import { IoIosMenu } from "react-icons/io";
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
+  const { user, logOut } = useAuth();
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        Swal.fire({
+          title: "Sign Out Successfully",
+          showClass: {
+            popup: `
+                animate__animated
+                animate__fadeInUp
+                animate__faster
+              `,
+          },
+          hideClass: {
+            popup: `
+                animate__animated
+                animate__fadeOutDown
+                animate__faster
+              `,
+          },
+        });
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: error.message,
+        });
+      });
+  };
+
   const navLinks = (
     <>
       <li>
         <a>Item 1</a>
       </li>
       <li>
-        <a>Item 3</a>
+        <Link to="/dashboard">Dashboard</Link>
       </li>
     </>
+
+    
   );
 
   return (
@@ -43,8 +79,24 @@ const Navbar = () => {
           <div className=" hidden lg:flex justify-end ">
             <ul className="menu menu-horizontal px-1 ">{navLinks}</ul>
           </div>
-          <div className="text-xl font-semibold text-teal-600">
-            <Link to="/login">Log In</Link>
+          <div className="font-semibold text-teal-600">
+          {user ? (
+          <div className="flex items-center gap-2 md:gap-4">
+            <div className="flex flex-col justify-center">
+              <img
+                className="h-8 w-6 md:h-8 md:w-10 rounded-full mx-auto"
+                src={user?.photoURL}
+                alt=""
+              />
+              <span>{user?.displayName}</span>
+            </div>
+            <button onClick={handleLogOut} >
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <Link to="/login">Log In</Link>
+        )}
           </div>
         </div>
       </div>
