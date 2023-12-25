131b1ae1a40eedc959f42521fe4768405ed2be09
rakibaakter <rakibaakter635@gmail.com>
2023-12-25 22:08:06 +0600

    created table for all list

--- src/Dashboard/AllTask/AllTask.jsx
+++ src/Dashboard/AllTask/AllTask.jsx
@@ -1,10 +1,89 @@
 
 import { Link } from 'react-router-dom';
 import PrimaryBtn from '../../Shared/Buttons/PrimaryBtn';
+import { useEffect, useState } from 'react';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import useAuth from '../../Hooks/useAuth';
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { RiEdit2Fill } from "react-icons/ri";
+
+
 
 const AllTask = () => {
+    const {user} = useAuth()
+    const axiosPublic = useAxiosPublic()
+    const [tasks, setTask] = useState([]);
+
+useEffect(()=>{
+    axiosPublic.get('/tasks')
+    .then(res =>{
+        const allData = res.data;
+        // console.log(allData);
+        const userData = allData.filter(data=> data.email === user.email)
+        setTask(userData);
+    })
+    .catch(err =>{
+        console.log(err.message);
+    })
+}, [])
+// console.log(tasks);
+
     return (
-        <div>
+        <div className="my-12">
+
+            <div>
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+        </th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Deadline</th>
+        <th>Priority</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            tasks.map((task, ind)=> <tr key={task._id} ind={ind}>
+                <th>
+                  {ind+1}
+                </th>
+                <td className="text-xl">
+                  {task.title}
+                </td>
+                <td>
+                  {task.description}
+                </td>
+                <td>
+                    {task.deadline}
+                </td>
+                <td>
+                    {task.priority}
+                </td>
+                <td>
+                    {task.status}
+                </td>
+                <th>
+                    <div >
+                    <button className="btn btn-ghost text-xl text-sky-500"><RiEdit2Fill /></button>
+                    <button className="btn btn-ghost text-xl text-red-600"><RiDeleteBin5Line /></button>
+                    </div>
+                  
+                </th>
+              </tr>)
+        }
+      
+    </tbody>
+   
+    
+  </table>
+</div>
+            </div>
+
             <Link to="/dashboard/create-task">
                 <PrimaryBtn title="Add New Task" />
             </Link>
--- src/Dashboard/CreateTask/CreateTask.jsx
+++ src/Dashboard/CreateTask/CreateTask.jsx
@@ -47,6 +47,7 @@ const CreateTask = () => {
                   `,
                 },
               });
+              reset()
             }
           })
           .catch((error) => {
